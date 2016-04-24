a929249cd8edcf035ecd397d07b0e4b66a3df4ff
Josef Ludvicek <jludvice@redhat.com>
2016-04-24 13:44:58 +0200

    Show view result in design doc

--- couchtacty/_attachments/script/app.js
+++ couchtacty/_attachments/script/app.js
@@ -1,5 +1,25 @@
 // Apache 2.0 J Chris Anderson 2011
-$(function() {   
+$(function () {
 
+    $('<ul id="studentList"/>').appendTo('#students');
 
- });
+    $.couch.db("mydb").view("couchtacty/list", {
+        success: function (data) {
+
+            var html = $.map(data.rows, function (value, index) {
+                //console.info(value.key);
+
+                var v = value.value;
+                return "<li>" + v.name + ' ' + v.surname + '(' + v.email + ")</li>";
+            });
+
+            $(html).appendTo('#studentList');
+
+        },
+        error: function (status) {
+            console.log(status);
+        },
+        reduce: false
+    });
+
+});
